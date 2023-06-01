2df650a6c66412af9b77db8894f47c6d8eaf0c95
saipratap9 <skondreddy@presidio.com>
2023-06-01 17:42:40 +0530

    created chime service and moved chime related code there

--- src/screens/Meeting/index.tsx
+++ src/screens/Meeting/index.tsx
@@ -9,10 +9,12 @@ import {
   MeetingSessionConfiguration,
 } from "amazon-chime-sdk-js";
 import { useLocation } from "react-router-dom";
+import ChimeService from "../../services/chimeService";
 
 const Meeting = () => {
   const [meetingSession, setMeetingSession] = useState<DefaultMeetingSession>();
   const [meetingInfo, setMeetingInfo] = useState(null);
+  const [chimeService, setChimeService] = useState();
 
   const params = useLocation();
   useEffect(() => {
@@ -26,35 +28,13 @@ const Meeting = () => {
   }, []);
 
   useEffect(() => {
-    selectAudioVideo().then((data) => bindAudioAndVideo());
+    if (meetingSession) {
+      const chimeService = new ChimeService(meetingSession);
+      chimeService
+        .selectAudioVideo()
+        .then((data) => chimeService.bindAudioAndVideo());
+    }
   }, [meetingSession]);
-
-  const audioVideoObserver: any = {
-    audioInputsChanged: (freshAudioInputDeviceList: any) => {
-      // An array of MediaDeviceInfo objects
-      freshAudioInputDeviceList.forEach((mediaDeviceInfo: MediaDeviceInfo) => {
-        console.log(
-          `Device ID: ${mediaDeviceInfo.deviceId} Microphone: ${mediaDeviceInfo.label}`
-        );
-      });
-    },
-
-    audioOutputsChanged: (freshAudioOutputDeviceList: any) => {
-      console.log("Audio outputs updated: ", freshAudioOutputDeviceList);
-    },
-
-    videoInputsChanged: (freshVideoInputDeviceList: any) => {
-      console.log("Video inputs updated: ", freshVideoInputDeviceList);
-    },
-
-    audioInputMuteStateChanged: (device: MediaDeviceInfo, muted: boolean) => {
-      console.log(
-        "Device",
-        device,
-        muted ? "is muted in hardware" : "is not muted"
-      );
-    },
-  };
 
   async function createMeetingSession(meetingInfo: any) {
     const logger = new ConsoleLogger("MyLogger", LogLevel.INFO);
@@ -67,111 +47,6 @@ const Meeting = () => {
     setMeetingSession(
       new DefaultMeetingSession(configuration, logger, deviceController)
     );
-    meetingSession?.audioVideo.addDeviceChangeObserver(audioVideoObserver);
-  }
-
-  async function getAudioInputDevices(): Promise<
-    MediaDeviceInfo[] | undefined
-  > {
-    const audioInputDevices =
-      meetingSession?.audioVideo.listAudioInputDevices();
-
-    return audioInputDevices?.then((data) => {
-      console.log("Audio Input Devices data", data);
-      return data;
-    });
-  }
-
-  async function getAudioOutputDevices(): Promise<
-    MediaDeviceInfo[] | undefined
-  > {
-    const audioOutputDevices =
-      meetingSession?.audioVideo.listAudioOutputDevices();
-
-    return audioOutputDevices?.then((data) => {
-      console.log("Audio Output devices", data);
-      return data;
-    });
-  }
-
-  async function getVideoInputDevices(): Promise<
-    MediaDeviceInfo[] | undefined
-  > {
-    const videoInputDevices =
-      meetingSession?.audioVideo.listVideoInputDevices();
-    return videoInputDevices?.then((data) => {
-      console.log("Video InputDevices devices", data);
-      return data;
-    });
-  }
-
-  // selecting audio input and audio output devices
-  // selecting video input devices
-  async function selectAudioVideo() {
-    console.log("Select audio video");
-    getAudioInputDevices().then((audioInputDevices) => {
-      if (audioInputDevices)
-        meetingSession?.audioVideo.startAudioInput(
-          audioInputDevices[0].deviceId
-        );
-    });
-    getAudioOutputDevices().then((audioOutputDevices) => {
-      if (audioOutputDevices && audioOutputDevices.length > 0)
-        meetingSession?.audioVideo.chooseAudioOutput(
-          audioOutputDevices[0].deviceId
-        );
-    });
-
-    getVideoInputDevices().then((videoInputDevices) => {
-      if (videoInputDevices && videoInputDevices.length > 0) {
-        meetingSession?.audioVideo
-          .startVideoInput(videoInputDevices[0].deviceId)
-          .then((data) => {
-            // it will turn off indicating the camera is no longer capturing.
-            // meetingSession?.audioVideo.stopVideoInput();
-          });
-      }
-    });
-  }
-
-  async function bindAudioAndVideo() {
-    console.log("bind audio video");
-    const myAudioElement = document.getElementById(
-      "myAudio"
-    ) as HTMLAudioElement;
-    const myVideoElement = document.getElementById(
-      "myVideo"
-    ) as HTMLVideoElement;
-
-    if (myAudioElement) {
-      console.log("My audio element");
-      meetingSession?.audioVideo.bindAudioElement(myAudioElement);
-    }
-
-    if (myVideoElement)
-      meetingSession?.audioVideo.bindVideoElement(1, myVideoElement);
-
-    const observer = {
-      audioVideoDidStart: () => {
-        console.log("Started");
-      },
-      // videoTileDidUpdate is called whenever a new tile is created or tileState changes.
-      videoTileDidUpdate: (tileState: any) => {
-        // Ignore a tile without attendee ID and other attendee's tile.
-        if (!tileState.boundAttendeeId || !tileState.localTile) {
-          return;
-        }
-
-        meetingSession?.audioVideo.bindVideoElement(
-          tileState.tileId,
-          myVideoElement
-        );
-      },
-    };
-
-    meetingSession?.audioVideo.addObserver(observer);
-    meetingSession?.audioVideo.start();
-    meetingSession?.audioVideo.startLocalVideoTile();
   }
 
   return (
--- src/services/chimeService.ts
+++ src/services/chimeService.ts
@@ -1,5 +1,134 @@
 import { DefaultMeetingSession } from "amazon-chime-sdk-js";
 
 class ChimeService {
-  constructor(private meetingSession: DefaultMeetingSession) {}
+  public audioVideoObserver: any;
+  constructor(private meetingSession: DefaultMeetingSession) {
+    this.audioVideoObserver = {
+      audioInputsChanged: (freshAudioInputDeviceList: any) => {
+        // An array of MediaDeviceInfo objects
+        freshAudioInputDeviceList.forEach(
+          (mediaDeviceInfo: MediaDeviceInfo) => {
+            console.log(
+              `Device ID: ${mediaDeviceInfo.deviceId} Microphone: ${mediaDeviceInfo.label}`
+            );
+          }
+        );
+      },
+
+      audioOutputsChanged: (freshAudioOutputDeviceList: any) => {
+        console.log("Audio outputs updated: ", freshAudioOutputDeviceList);
+      },
+
+      videoInputsChanged: (freshVideoInputDeviceList: any) => {
+        console.log("Video inputs updated: ", freshVideoInputDeviceList);
+      },
+
+      audioInputMuteStateChanged: (device: MediaDeviceInfo, muted: boolean) => {
+        console.log(
+          "Device",
+          device,
+          muted ? "is muted in hardware" : "is not muted"
+        );
+      },
+    };
+
+    this.meetingSession?.audioVideo.addDeviceChangeObserver(
+      this.audioVideoObserver
+    );
+  }
+
+  async getAudioInputDevices(): Promise<MediaDeviceInfo[] | undefined> {
+    const audioInputDevices =
+      this.meetingSession?.audioVideo.listAudioInputDevices();
+
+    return audioInputDevices?.then((data) => {
+      console.log("Audio Input Devices data", data);
+      return data;
+    });
+  }
+
+  async getAudioOutputDevices(): Promise<MediaDeviceInfo[] | undefined> {
+    const audioOutputDevices =
+      this.meetingSession?.audioVideo.listAudioOutputDevices();
+
+    return audioOutputDevices?.then((data: any) => {
+      console.log("Audio Output devices", data);
+      return data;
+    });
+  }
+
+  async getVideoInputDevices(): Promise<MediaDeviceInfo[] | undefined> {
+    const videoInputDevices =
+      this.meetingSession?.audioVideo.listVideoInputDevices();
+    return videoInputDevices?.then((data) => {
+      console.log("Video InputDevices devices", data);
+      return data;
+    });
+  }
+
+  // selecting audio input and audio output devices
+  // selecting video input devices
+  async selectAudioVideo() {
+    console.log("Select audio video");
+    this.getAudioInputDevices().then((audioInputDevices) => {
+      if (audioInputDevices)
+        this.meetingSession?.audioVideo.startAudioInput(
+          audioInputDevices[0].deviceId
+        );
+    });
+    this.getAudioOutputDevices().then((audioOutputDevices) => {
+      if (audioOutputDevices && audioOutputDevices.length > 0)
+        this.meetingSession?.audioVideo.chooseAudioOutput(
+          audioOutputDevices[0].deviceId
+        );
+    });
+
+    this.getVideoInputDevices().then((videoInputDevices) => {
+      if (videoInputDevices && videoInputDevices.length > 0) {
+        this.meetingSession?.audioVideo
+          .startVideoInput(videoInputDevices[0].deviceId)
+          .then((data) => {
+            // it will turn off indicating the camera is no longer capturing.
+            // meetingSession?.audioVideo.stopVideoInput();
+          });
+      }
+    });
+  }
+
+  async bindAudioAndVideo(videoId = "myVideo", audioId = "myAudio") {
+    console.log("bind audio video");
+    const myAudioElement = document.getElementById(audioId) as HTMLAudioElement;
+    const myVideoElement = document.getElementById(videoId) as HTMLVideoElement;
+
+    if (myAudioElement) {
+      this.meetingSession?.audioVideo.bindAudioElement(myAudioElement);
+    }
+
+    if (myVideoElement)
+      this.meetingSession?.audioVideo.bindVideoElement(1, myVideoElement);
+
+    const observer = {
+      audioVideoDidStart: () => {
+        console.log("Started");
+      },
+      // videoTileDidUpdate is called whenever a new tile is created or tileState changes.
+      videoTileDidUpdate: (tileState: any) => {
+        // Ignore a tile without attendee ID and other attendee's tile.
+        if (!tileState.boundAttendeeId || !tileState.localTile) {
+          return;
+        }
+
+        this.meetingSession?.audioVideo.bindVideoElement(
+          tileState.tileId,
+          myVideoElement
+        );
+      },
+    };
+
+    this.meetingSession?.audioVideo.addObserver(observer);
+    this.meetingSession?.audioVideo.start();
+    this.meetingSession?.audioVideo.startLocalVideoTile();
+  }
 }
+
+export default ChimeService;
